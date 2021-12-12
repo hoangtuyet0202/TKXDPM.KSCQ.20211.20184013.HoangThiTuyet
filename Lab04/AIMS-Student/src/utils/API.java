f697aa4553e1d18072ddd72ebf7f6ba335be1e1d
hoang tuyet <hoangtuyetkn@gmail.com>
2021-12-12 19:39:14 +0700

    update lab 4

--- Lab04/AIMS-Student/src/utils/API.java
+++ Lab04/AIMS-Student/src/utils/API.java
@@ -9,6 +9,7 @@ import java.io.Writer;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -20,82 +21,140 @@ import java.util.logging.Logger;
 import entity.payment.CreditCard;
 import entity.payment.PaymentTransaction;
 
-public class API {
+/**
+ * Represent an API call
+ * @author tuyetht
+ * @version 1.1
+ * Date: 12/12/2021
+ */
+public class API{
 
-	public static DateFormat DATE_FORMATER = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-	private static Logger LOGGER = Utils.getLogger(Utils.class.getName());
+    /**
+     * A date formatter to format all date to yyyy/MM/dd HH:mm:ss 
+     */
+    public static DateFormat DATE_FORMATER = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
-	public static String get(String url, String token) throws Exception {
-		LOGGER.info("Request URL: " + url + "\n");
-		URL line_api_url = new URL(url);
-		HttpURLConnection conn = (HttpURLConnection) line_api_url.openConnection();
-		conn.setDoInput(true);
-		conn.setDoOutput(true);
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Content-Type", "application/json");
-		conn.setRequestProperty("Authorization", "Bearer " + token);
-		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		String inputLine;
-		StringBuilder respone = new StringBuilder(); // ising StringBuilder for the sake of memory and performance
-		while ((inputLine = in.readLine()) != null)
-			System.out.println(inputLine);
-		respone.append(inputLine + "\n");
-		in.close();
-		LOGGER.info("Respone Info: " + respone.substring(0, respone.length() - 1).toString());
-		return respone.substring(0, respone.length() - 1).toString();
-	}
+    /**
+     * Common Logger of class
+     */
+    private static final Logger LOGGER = Utils.getLogger(Utils.class.getName());
 
-	int var;
+    /**
+     * Get API 
+     *
+     * @param url   url of api
+     * @param token secret token
+     * @return not known yet
+     * @throws Exception
+     */
+    public static String get(String url, String token) throws Exception{
+        // set up
 
-	public static String post(String url, String data
-//			, String token
-	) throws IOException {
-		allowMethods("PATCH");
-		URL line_api_url = new URL(url);
-		String payload = data;
-		LOGGER.info("Request Info:\nRequest URL: " + url + "\n" + "Payload Data: " + payload + "\n");
-		HttpURLConnection conn = (HttpURLConnection) line_api_url.openConnection();
-		conn.setDoInput(true);
-		conn.setDoOutput(true);
-		conn.setRequestMethod("PATCH");
-		conn.setRequestProperty("Content-Type", "application/json");
-//		conn.setRequestProperty("Authorization", "Bearer " + token);
-		Writer writer = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-		writer.write(payload);
-		writer.close();
-		BufferedReader in;
-		String inputLine;
-		if (conn.getResponseCode() / 100 == 2) {
-			in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		} else {
-			in = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-		}
-		StringBuilder response = new StringBuilder();
-		while ((inputLine = in.readLine()) != null)
-			response.append(inputLine);
-		in.close();
-		LOGGER.info("Respone Info: " + response.toString());
-		return response.toString();
-	}
+        HttpURLConnection conn = setUpConnection(url, "GET", token);
 
-	private static void allowMethods(String... methods) {
-		try {
-			Field methodsField = HttpURLConnection.class.getDeclaredField("methods");
-			methodsField.setAccessible(true);
+        // return result after processing
+        return readResponse(conn);
+    }
 
-			Field modifiersField = Field.class.getDeclaredField("modifiers");
-			modifiersField.setAccessible(true);
-			modifiersField.setInt(methodsField, methodsField.getModifiers() & ~Modifier.FINAL);
+    int var;
 
-			String[] oldMethods = (String[]) methodsField.get(null);
-			Set<String> methodsSet = new LinkedHashSet<>(Arrays.asList(oldMethods));
-			methodsSet.addAll(Arrays.asList(methods));
-			String[] newMethods = methodsSet.toArray(new String[0]);
+    /**
+     * post APi 
+     *
+     * @param url   API url
+     * @param data  data to send
+     * @param token secret token
+     * @return not known yet
+     * @throws IOException
+     */
+    public static String post(String url, String data, String token) throws IOException{
+        // set up
+        allowMethods("PATCH");
+        HttpURLConnection conn = setUpConnection(url, "POST", token);
 
-			methodsField.set(null/* static field */, newMethods);
-		} catch (NoSuchFieldException | IllegalAccessException e) {
-			throw new IllegalStateException(e);
-		}
-	}
+        // send data
+        Writer writer = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+        writer.write(data);
+        writer.close();
+
+        return readResponse(conn);
+    }
+
+    /** 
+     * @param methods allowed method (POST, PATCH ...)
+     * @deprecated only worked with java <= 11
+     */
+    private static void allowMethods(String... methods){
+        try{
+            Field methodsField = HttpURLConnection.class.getDeclaredField("methods");
+            methodsField.setAccessible(true);
+
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(methodsField, methodsField.getModifiers()&~Modifier.FINAL);
+
+            String[] oldMethods = (String[]) methodsField.get(null);
+            Set<String> methodsSet = new LinkedHashSet<>(Arrays.asList(oldMethods));
+            methodsSet.addAll(Arrays.asList(methods));
+            String[] newMethods = methodsSet.toArray(new String[0]);
+
+            methodsField.set(null/* static field */, newMethods);
+        }catch(NoSuchFieldException|IllegalAccessException e){
+            throw new IllegalStateException(e);
+        }
+    }
+
+    /**
+     * Prepare connection to call
+     * 
+     * @param url    api url
+     * @param method POST, GET, PATCH ...
+     * @param token  secret token
+     * @return HttpUrlConnection
+     * @throws IOException
+     */
+    private static HttpURLConnection setUpConnection(String url, String method, String token) throws IOException{
+        LOGGER.info("Request URL: " + url + "\n");
+        URL apiUrl = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) apiUrl.openConnection();
+
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setRequestMethod(method);
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("Authorization", "Bearer ".concat(token));
+
+        return conn;
+
+    }
+
+    /** 
+     * Read response from HttpConnection after request
+     * @param conn the connection
+     * @return response string
+     * @throws IOException
+     */
+    private static String readResponse(HttpURLConnection conn) throws IOException{
+        BufferedReader in;
+        String inputLine;
+
+        if(conn.getResponseCode() == 200){
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        }else{
+            in = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        // use string builder for performance
+        StringBuilder response = new StringBuilder();
+        while((inputLine = in.readLine()) != null){
+            System.out.println(inputLine);
+            response.append(inputLine).append("\n");
+        }
+
+        in.close();
+
+        LOGGER.info("Response info: " + response);
+
+        return response.toString();
+    }
 
 }
